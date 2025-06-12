5444e255afb2f1790bbecb7350c07efd36e7d027
amir <alphaelite10@gmail.com>
2025-06-12 14:39:17 +0545

    comparison

--- utils/feature_comparison.py
+++ utils/feature_comparison.py
@@ -1,47 +1,58 @@
 import os
 import torch
+import pandas as pd
 import torch.nn.functional as F
 
 from feature_extraction import extract_features, load_model, load_tensor
 from segmentation import create_tensor, watershed_segmentation, load_medical_volume
 
-if __name__=="__main":
+
+if __name__=="__main__":
 
     # Load the 3D densenet121 model
     model_3d = load_model("model_3d.pth")
 
-    mask_path = os.environ.get("MASK_DATA_PATH")
+    tibia_3d = load_tensor("tibia_3d.pth")
 
-    mask_data, _ = load_medical_volume(mask_path)
+    femur_3d = load_tensor("femur_3d.pth")
 
-    labels = watershed_segmentation(mask_data)
+    background_3d = load_tensor("background_3d.pth")
 
-    tibia_3d = create_tensor(labels, "tibia")
-
-    femur_3d = create_tensor(labels, "femur")
-
-    background_3d = create_tensor(labels, "background_3d")
-
-    layer_names = [
+    layer_names = {
         
-        "features.denseblock4.denselayer16.conv2",
-        "features.denseblock4.denselayer15.conv2",
-        "features.denseblock4.denselayer14.conv2"
-    ]
-
-    features = {
-        
-        "tibia": extract_features(model_3d, tibia_3d, layer_names),
-
-        # "femur": extract_features(model_3d, femur_3d, layer_names),
-        
-        # "background" : extract_features(model_3d, background_3d, layer_names)
-
+        "last": "features.denseblock4.denselayer16.conv2",
+        "third-last":"features.denseblock4.denselayer15.conv2",
+        "fifth-last":"features.denseblock4.denselayer14.conv2"
     }
 
-    for name, ndim in features.items():
-        for _, feature in ndim.items():
-            print(feature)
+    # Region pairs
+    region_pairs = [
+        ("tibia", "femur"),
+        ("tibia", "background"),
+        ("femur", "background")
+    ]
 
-    cos_sim = F.cosine_similarity(f1, f2, dim=1)
-    print(f"Cosine similarity: {cos_sim.item():.4f}")
+    # Feature lookup
+    feature_lookup = {
+        "tibia": extract_features(model_3d, tibia_3d, layer_names.values()),
+        "femur": extract_features(model_3d, femur_3d, layer_names.values()),
+        "background": extract_features(model_3d, background_3d, layer_names.values())
+    }
+
+    # Create a list of rows
+    data = []
+
+    # Loop through region pairs and compute cosine similarity
+    for r1, r2 in region_pairs:
+        row = {}
+        row["Image Pair"] = f"{r1} <-> {r2}"
+        for pos, layer in layer_names.items():
+            f1 = feature_lookup[r1][layer]
+            f2 = feature_lookup[r2][layer]
+            sim = F.cosine_similarity(f1, f2, dim=1).item()
+            row[pos] = sim
+        data.append(row)
+
+    df = pd.DataFrame(data)
+
+    df.to_csv(os.getenv("STORE_LOCATION")+"similarity.csv")
